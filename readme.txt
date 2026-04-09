c12a4b2847e6e31d40596e0da89e54554378a5fc
Thomas Guillot <thomas@automattic.com>
2026-04-09 20:45:32 +0100

    feat: scaffold simple-graphs plugin

---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== Simple Graphs ===
+Contributors: you
+Tags: block, chart, gutenberg, infographic
+Requires at least: 6.4
+Tested up to: 6.7
+Stable tag: 0.1.0
+License: GPLv2 or later
+
+A Gutenberg block for creating visually-striking percentage charts.
---
+++ simple-graphs.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Simple Graphs
+ * Description: A Gutenberg block for creating visually-striking percentage charts.
+ * Version: 0.1.0
+ * Requires at least: 6.4
+ * Requires PHP: 7.4
+ * License: GPL-2.0-or-later
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+add_action(
+	'init',
+	function () {
+		register_block_type( __DIR__ . '/build' );
+	}
+);
