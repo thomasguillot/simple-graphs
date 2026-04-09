c41064ef61a6bcc239c8d3b3b49814195d62ab5c
Thomas Guillot <thomas@automattic.com>
2026-04-09 21:00:40 +0100

    fix: move block.json into src so it's copied to build/

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '874a77b492eac1d545da');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'aecaca777b0b1ad3a8ff');
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { registerBlockType, registerBlockStyle } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
-import metadata from '../block.json';
+import metadata from './block.json';
 import edit from './edit';
 import save from './save';
 import './style.scss';
