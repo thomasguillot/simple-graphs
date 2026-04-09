97f7b834a67727167aab26ec74a761711740662a
Thomas Guillot <thomas@automattic.com>
2026-04-09 21:22:00 +0100

    feat: set block icon to chartBar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import { registerBlockType, registerBlockStyle } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
+import { chartBar } from '@wordpress/icons';
 import metadata from './block.json';
 import edit from './edit';
 import save from './save';
 import './style.scss';
 import './editor.scss';
 
-registerBlockType( metadata.name, { edit, save } );
+registerBlockType( metadata.name, { icon: chartBar, edit, save } );
 
 [
 	{ name: 'column', label: __( 'Column', 'simple-graphs' ), isDefault: true },
