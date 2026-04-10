6fe67511cf41795f1c50ba56afb4caa2cf3f7703
Thomas Guillot <thomas@automattic.com>
2026-04-10 12:08:26 +0100

    refactor: legend item styles in CSS, gap uses spacing preset 20

--- src/charts/Legend.js
+++ src/charts/Legend.js
@@ -2,10 +2,6 @@ import { Icon as IconComponent } from '@wordpress/components';
 import { getIcon } from '../icons';
 import { contrastColor } from './shared';
 
-const RECT_SIZE = 32;
-const ICON_SIZE = 24;
-const GAP = 8;
-
 export default function Legend( { items } ) {
 	return (
 		<div className="simple-graphs-legend">
@@ -15,36 +11,20 @@ export default function Legend( { items } ) {
 					<div
 						key={ item.id }
 						className="simple-graphs-legend__item"
-						style={ {
-							display: 'flex',
-							alignItems: 'center',
-							gap: GAP,
-						} }
 					>
 						<span
-							style={ {
-								display: 'inline-flex',
-								alignItems: 'center',
-								justifyContent: 'center',
-								width: RECT_SIZE,
-								height: RECT_SIZE,
-								borderRadius: 6,
-								background: item.color,
-								flexShrink: 0,
-							} }
+							className="simple-graphs-legend__swatch"
+							style={ { background: item.color } }
 						>
 							{ icon && (
 								<IconComponent
 									icon={ icon }
-									size={ ICON_SIZE }
+									size={ 24 }
 									style={ { fill: contrastColor( item.color ) } }
 								/>
 							) }
 						</span>
-						<span
-							className="simple-graphs-legend__label"
-							style={ { fontSize: 13, color: '#374151' } }
-						>
+						<span className="simple-graphs-legend__label">
 							{ item.title }
 						</span>
 					</div>
