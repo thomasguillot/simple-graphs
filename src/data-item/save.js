c870ef1e55a9e4e24232fac8a1f674f6ed5fd8fa
Thomas Guillot <thomas@automattic.com>
2026-04-11 09:17:56 +0100

    feat: frontend rendering with prefix/suffix formatting

--- src/data-item/save.js
+++ src/data-item/save.js
@@ -1,23 +1,3 @@
-import { useBlockProps, RichText } from '@wordpress/block-editor';
-
-export default function save( { attributes } ) {
-	const { value, title } = attributes;
-	const blockProps = useBlockProps.save();
-
-	return (
-		<div { ...blockProps }>
-			<RichText.Content
-				tagName="span"
-				className="simple-graphs-data-item__value"
-				value={ value }
-			/>
-			{ title && (
-				<RichText.Content
-					tagName="span"
-					className="simple-graphs-data-item__title"
-					value={ title }
-				/>
-			) }
-		</div>
-	);
+export default function save() {
+	return null;
 }
